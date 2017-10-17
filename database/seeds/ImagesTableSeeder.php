2d892f2e300566a67deb511c73d405ed9be9fa53
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-10-17 10:30:48 +0100

    Getting Started with Migration, Model and Seeder (Faker)

---
+++ database/seeds/ImagesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Image;
+use Faker\Factory as Faker; 
+
+class ImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        $imageLinks = array(
+        	"https://jenniferjara.github.io/miportafolio/assets/img/iconos/css.png",
+        	"https://jenniferjara.github.io/miportafolio/assets/img/iconos/html.png",
+        	"https://jenniferjara.github.io/miportafolio/assets/img/iconos/jquery.png",
+        	"https://jenniferjara.github.io/miportafolio/assets/img/iconos/bootstrap.png",
+        );
+
+        foreach ($imageLinks as $imageLink) 
+        {
+        	Image::create([
+        		'title' => $faker->text(80),
+        		'description' => $content = $faker->paragraph(18),
+        		'thumbnail' => $imageLink,
+        		'imageLink' => $imageLink,
+        		'user_id' => $faker->numberBetween($min = 1, $max = 5),
+           	]);
+        }
+    }
+}
